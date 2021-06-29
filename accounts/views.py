fc2336ce6cfc029233292a099ba68dfb5363cc1f
Trevor Lesley <tlesley96@gmail.com>
2021-06-29 15:30:52 -0400

    Saving permissions changes

--- accounts/views.py
+++ accounts/views.py
@@ -2,8 +2,18 @@ from rest_framework import generics
 
 from .models import Profile
 from .serializers import ProfileSerializer
+from .permissions import IsAuthOrReadOnly
 
 
 class ProfileListAPIView(generics.ListCreateAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = (IsAuthOrReadyOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class ProfileDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
