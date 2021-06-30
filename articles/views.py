1a075b50e384ae5304c2310b6e90c1c4b74a84af
Trevor Lesley <tlesley96@gmail.com>
2021-06-30 10:18:46 -0400

    Saving progress, working on article views.

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Article
+
+from .permissions import IsAuthOrReadOnly
+from .serializers import ArticleSerializer
 
 # Create your views here.
+
+
+class ArticleReadAPIView(generics.ListAPIView):
+    queryset = get.objects.filter()
--- conf/settings.py
+++ conf/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -99,12 +100,22 @@ WSGI_APPLICATION = "conf.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+if os.environ.get("DATABASE_URL"):
+    DATABASES = {"default": dj_database_url.config(default=os.enciron["DATABASE_URL"])}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
     }
-}
 
 
 # Password validation
